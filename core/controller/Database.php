a2a955cfec072554e84bb39121febca9194a6c9f
JulenOZegibide <43949194+JulenOZegibide@users.noreply.github.com>
2020-01-07 13:12:16 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,7 +3,7 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="bookmedik";
+		$this->user="ba2c66054f4ce5";$this->pass="e84f8ad3";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_9f80669238ae6fc";
 	}
 
 	function connect(){
