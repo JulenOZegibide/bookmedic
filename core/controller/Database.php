99ae65fcd3f9c7bd9595ed48cc1a68f8e94e8764
JulenOZegibide <43949194+JulenOZegibide@users.noreply.github.com>
2020-02-07 08:40:15 +0100

    Update Database.php

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 class Database {
 	public static $db;
@@ -23,29 +23,3 @@ class Database {
 	
 }
 ?>
-=======
-<?php
-class Database {
-	public static $db;
-	public static $con;
-	function Database(){
-		$this->user="b960afdba24807";$this->pass="180592c5";$this->host="eu-cdbr-west-02.cleardb.net";$this->ddbb="heroku_119ea448af80abc";
-	}
-
-	function connect(){
-		$con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
-		$con->query("set sql_mode=''");
-		return $con;
-	}
-
-	public static function getCon(){
-		if(self::$con==null && self::$db==null){
-			self::$db = new Database();
-			self::$con = self::$db->connect();
-		}
-		return self::$con;
-	}
-	
-}
-?>
->>>>>>> master
